37a6fd7547e9ac84422477efcd2dea10e26611c9
stfzz <sirsapperlot@pm.me>
2022-05-16 13:36:09 +0200

    d

--- tagesmutter23.py
+++ tagesmutter23.py
@@ -10,7 +10,7 @@ from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 st.set_page_config(
-    page_title="Controllo Errori TAGESMÜTTER", layout="wide", page_icon="😁"  # 👽
+    page_title="Controllo Errori TAGESMÜTTER", layout="wide", page_icon="👽"  # 👽
 )
 
 DATAINIZIOMINIMA = pd.to_datetime("18.05.2020", format="%d.%m.%Y")
