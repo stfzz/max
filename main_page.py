920643240143d0408538ee9065a150a9d16ba669
stfzz <sirsapperlot@pm.me>
2022-06-20 10:31:36 +0200

    now multipage

---
+++ main_page.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# Pagina principale 🎈")
+st.sidebar.markdown("# Pagina principale 🎈")
---
+++ pages/BetriebsKitas.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.markdown("# In arrivo la versione per le Kitas aziendali 😀")
